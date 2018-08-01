3c0ff0164ae9a8bedf697e0684ddaa95227998be
Michal Szewczak <michal@szewczak.email>
2018-08-01 18:19:07 +0100

    add deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule ReverseProxyPlug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:plug, "~> 1.6"},
+      {:cowboy, "~> 2.4"},
+      {:httpoison, "~> 1.2"},
+      {:credo, "~> 0.5", only: [:dev, :test]}
     ]
   end
 end
