a975d3cc7c89ffbb3be5657e8f0f1f60bb5fde07
Michal Szewczak <michal@szewczak.email>
2018-08-06 16:43:24 +0200

    add first test

--- test/reverse_proxy_test.exs
+++ test/reverse_proxy_test.exs
@@ -4,11 +4,34 @@ defmodule ReverseProxyTest do
 
   import Mox
 
-  test "greets the world" do
-    ReverseProxy.HTTPClientMock
-    |> expect(:request, fn _a, _b, _c, _d, _e -> :ok end)
+  defp get_responder(status, headers, body, no_chunks \\ 1) do
+    fn _a, _b, _c, _d, _e ->
+      send(self(), %HTTPoison.AsyncStatus{code: status})
+      send(self(), %HTTPoison.AsyncHeaders{headers: headers})
 
-    conn(:get, "/")
-    |> ReverseProxy.call(upstream: "example.com")
+      body
+      |> String.codepoints()
+      |> Enum.chunk_every(body |> String.length() |> div(no_chunks))
+      |> Enum.map(&Enum.join/1)
+      |> Enum.map(fn chunk ->
+        send(self(), %HTTPoison.AsyncChunk{chunk: chunk})
+      end)
+
+      send(self(), %HTTPoison.AsyncEnd{})
+      nil
+    end
+  end
+
+  test "receives response" do
+    ReverseProxy.HTTPClientMock
+    |> expect(:request, get_responder(200, [{"host", "example.com"}], "Success", 2))
+
+    conn =
+      conn(:get, "/")
+      |> ReverseProxy.call(upstream: "example.com")
+
+    assert conn.status == 200
+    assert Enum.member?(conn.resp_headers, {"host", "example.com"})
+    assert conn.resp_body == "Success"
   end
 end
