37479ea55caf180c6a5bf7930b72edca0496779a
Michal Szewczak <michal@szewczak.email>
2018-10-10 14:08:14 +0100

    v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReverseProxyPlug.MixProject do
   def project do
     [
       app: :reverse_proxy_plug,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
