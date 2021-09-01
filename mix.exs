49dc3b5d264db0b007a1eb36ba5cfb3911b53dab
Sam Nipps <sam@tallarium.com>
2021-09-01 14:57:10 +0100

    v2.0.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ReverseProxyPlug.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/tallarium/reverse_proxy_plug"
-  @version "1.5.0"
+  @version "2.0.0"
 
   def project do
     [
